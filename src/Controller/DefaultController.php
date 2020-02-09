505789df0b6473dcab13c71ccae814e1178612d6
soizic Lambert <soizic.lee@gmail.com>
2020-02-09 21:55:52 +0100

    quete 5 - add routing avance

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,10 +1,25 @@
 <?php
-
+//src/Controller/DefaultController.php
 
 namespace App\Controller;
 
 
-class DefaultController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
 {
+
+    /**
+     * @Route("/", name="app_index")
+     * @return Response
+     */
+    public function index(): Response
+    {
+
+        return $this->render('home.html.twig');
+    }
+
 
 }
--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -1,1 +1,49 @@
 <?php
+//src/ControllerWildController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WildController extends AbstractController
+
+    /**
+     * @Route("/wild", name="wild_index")
+     */
+
+{
+    /**
+     * @return Response
+     */
+    public function index() :Response
+    {
+
+        return $this -> render ('wild/home.html.twig' ,
+            [
+                'website' => 'Wild Séries',
+            ]);
+    }
+
+    /**
+     * @Route("/show/{slug}",  name="wild_show",
+     *     requirements={"slug"="[a-z0-9\-]+"},
+     *     defaults={"slug" = ""})
+     * @return Response
+     */
+    public function show(string $slug) : Response
+    {
+        if (!$slug) {
+           throw $this
+                ->createNotFoundException('Aucune série sélectionnée, veuillez choisir une série');
+        }
+
+        $slug = ucwords(str_replace('-', ' ', $slug));
+
+        return $this -> render ( 'wild/show.html.twig', [
+            'slug' => $slug,
+        ]);
+
+    }
+}
+
